a57a2933bedae2a3b3dc9a91d06ef48b98d45e68
Sebastiaan-K <142668125+Sebastiaan-K@users.noreply.github.com>
2023-11-06 07:50:10 +0100

    final push

---
+++ LAB SQL Subqueries.sql
@@ -0,0 +1,22 @@
+USE sakila;
+
+SHOW TABLES;
+
+#1 Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT COUNT(inventory_id) 
+FROM inventory 
+WHERE film_id = (SELECT film_id FROM film WHERE title = 'Hunchback Impossible');
+
+#2 List all films whose length is longer than the average length of all the films in the Sakila database.
+SELECT title, length 
+FROM film 
+WHERE length > (SELECT AVG(length) FROM film);
+
+#3 Use a subquery to display all actors who appear in the film "Alone Trip".
+SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE actor_id IN (
+    SELECT actor_id 
+    FROM film_actor 
+    WHERE film_id = (SELECT film_id FROM film WHERE title = 'Alone Trip')
+);
